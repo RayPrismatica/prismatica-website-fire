0e45a8fe05dbe462ea7a32b0449f2b6c258b3870
Prismaticalab <ray@prismaticalab.com>
2025-11-16 09:32:16 +0100

    fix: add useEffect import to ProductsEnquiryModal

--- components/ProductsEnquiryModal.tsx
+++ components/ProductsEnquiryModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
 interface ProductsEnquiryModalProps {
